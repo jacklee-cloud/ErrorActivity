4286577e952ea1c76d6fdadc9229c8d690c551cd
liyang <876204573@qq.com>
2019-11-26 19:43:27 +0800

    The error activity of Android

---
+++ app/src/main/java/com/example/testactivity/FirstActivity.java
@@ -0,0 +1,45 @@
+package com.example.testactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1=(Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"你傻了",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"我傻了",Toast.LENGTH_SHORT).show();
+                break;
+                default:
+        }
+        return true;
+    }
+}
